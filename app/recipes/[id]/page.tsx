94f685c48590deeef8d560a155193b5a4b19b361
Ashraf-Swaidan <ashraf.swaidan.10@gmail.com>
2024-11-13 01:43:28 +0200

    hmm

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -3,14 +3,14 @@ import RecipeDetailedCard from '@/app/components/recipe/RecipeDetailedCard';
 import RecipeDetailedCardSkeleton from '@/app/components/skeletons/RecipeDetailedCardSkeleton';
 
 interface PageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
-const Page = ({ params }: PageProps) => {
-  const { id } = params;
+const Page = async ({ params }: PageProps) => {
+  const { id } = await params;
 
   return (
     <div className="min-h-screen bg-orange-50 py-8 px-4 sm:px-6 lg:px-8">
