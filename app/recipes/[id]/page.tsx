84670192bbe0a037ec2b75910c066b6358db6021
Ashraf-Swaidan <ashraf.swaidan.10@gmail.com>
2024-11-13 01:49:38 +0200

    attempting what was found in the documentation

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -1,16 +1,14 @@
 import React, { Suspense } from 'react'
 import RecipeDetailedCard from '@/app/components/recipe/RecipeDetailedCard';
 import RecipeDetailedCardSkeleton from '@/app/components/skeletons/RecipeDetailedCardSkeleton';
+import type { Metadata, ResolvingMetadata } from 'next'
 
-interface PageProps {
-  params: Promise<{
-    id: string;
-  }>;
-  searchParams: { [key: string]: string | string[] | undefined };
+type Props = {
+  params: Promise<{ id: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
-
-const Page = async ({ params }: PageProps) => {
-  const { id } = await params;
+const Page = async ({ params }: Props, parent: ResolvingMetadata) => {
+  const id = (await params).id
 
   return (
     <div className="min-h-screen bg-orange-50 py-8 px-4 sm:px-6 lg:px-8">
